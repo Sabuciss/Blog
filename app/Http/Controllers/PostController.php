30f8bf7681cb2637b38fc50122c937b6750350d0
unknown <ipb23.s.brikmane@vtdt.edu.lv>
2025-03-27 09:44:37 +0200

    cpanel yipii

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -38,7 +38,7 @@ class PostController extends Controller
 
     public function show(Post $post)
     {
-        $post->load('comments');
+        $post->load(['category', 'comments']); 
         return view("posts.show", compact("post"));
     }
 
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,22 +1,25 @@
+
 <x-layout>
   <x-slot:title>
     {{ $post->content }}
   </x-slot:title>
 
   <h1>{{ $post->content }}</h1>
-  <p>Kategorija: {{ $post->category->category_name }}</p>
+ <p>Kategorija: {{ optional($post->category)->category_name ?? 'Nav kategorijas' }}</p>
 
-  <a href="/posts/{{ $post->id }}/edit">Rediģē</a>
+  
+<a href="/posts/{{ $post->id }}/edit">Rediģē</a>
 
-  <form method="POST" action="/posts/{{ $post->id }}">
-    @csrf
-    @method("delete")
-    <button type="submit">Dzēst</button>
-  </form>
 
-  <br><br>
+<form method="POST" action="/posts/{{ $post->id }}">
+@csrf
+@method("delete")
+<button type="submit">dzēst</button>
+</form>
 
-  <form action="/comments" method="POST">
+<br><br>
+
+<form action="/comments" method="POST">
     @csrf
     <input type="hidden" name="post_id" value="{{ $post->id }}">
 
@@ -33,29 +36,32 @@
     </label>
 
     <button type="submit">Komentēt</button>
-  </form>
+</form>
 
-  @error('author')
-      <p>{{ $message }}</p>
-  @enderror
 
-  @error('comment')
-      <p>{{ $message }}</p>
-  @enderror
+    @error('author')
+        <p>{{ $message }}</p>
+    @enderror
 
-  <h2>Komentāri</h2>
-  @foreach ($post->comments as $comment)
-    <div>
-      <p><strong>{{ $comment->author }}</strong></p>
-      <p>{{ $comment->comment }}</p>
-      <a href="/comments/{{ $comment->id }}/edit">Rediģē</a>
+    @error('comment')
+            <p>{{ $message }}</p>
+    @enderror
 
-      <form method="POST" action="/comments/{{ $comment->id }}" style="display: inline;">
-        @csrf
-        @method("delete")
-        <button type="submit">Dzēst</button>
-      </form>
+
+<h2>Komentāri</h2>
+@foreach ($post->comments as $comment)
+    <div class="comment">
+        <p><strong>{{ $comment->author }}</strong></p>
+        <p>{{ $comment->comment }}</p>
+        <a href="/comments/{{ $comment->id }}/edit">Rediģē</a>
+
+        <form method="POST" action="/comments/{{ $comment->id }}">
+            @csrf
+            @method("delete")
+            <button type="submit">Dzēst</button>
+        </form>
     </div>
-  @endforeach
+@endforeach
 
+</form>
 </x-layout>
